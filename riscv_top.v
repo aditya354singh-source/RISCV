322be011c79b663605b53a5b32403fa2d2645c89
aditya354singh-source <aditya354singh@gmail.com>
2026-03-19 00:09:09 +0530

    Create riscv_top.v

---
+++ riscv_top.v
@@ -0,0 +1,14 @@
+module riscv_top(
+    input clk,
+    input reset,
+    output reg [31:0] pc
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        pc <= 0;
+    else
+        pc <= pc + 4;
+end
+
+endmodule
